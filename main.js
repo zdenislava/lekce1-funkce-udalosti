f566f7aa03a0ff23066e6c1801c19e7aa4a585a6
zdenislava <navarikova@seznam.cz>
2021-03-12 18:03:11 +0100

    ukol 1,2,3 hotovy

--- main.js
+++ main.js
@@ -1,25 +1,38 @@
 // tady je místo pro náš program
 
-document.querySelector("#vysledek").innerHTML = secti(4, 5);
-
+function vysledek() {
+  document.querySelector("#vysledek").innerHTML = secti(4, 5);
+}
+  
 /**
  * Sečte dvě čísla na vstupu a vrátí výsledek
  *
  * @param {int} a
  * @param {int} b
  */
-function secti(a, b) {
+
+
+  function secti(a, b) {
   let c = a + b;
   return c;
-}
+  }
+
 
 /**
- * Upozorní uživatele při spuštění.
+ * Upozorní uživatele při spuštění + vypisuje text do ctverecku a console
  */
+
 function upozorni() {
-  alert("Gratulace, právě jsi spustila tuto funkci!");
+  let upozorneni = "Gratulace, právě jsi spustila tuto funkci!"
+  let ctverecek;
+  ctverecek = document.querySelector('.ctverecek');
+  alert(upozorneni);
+  console.log(upozorneni);
+  ctverecek.style.fontSize = "8px";
+  ctverecek.textContent = upozorneni;
 }
 
+//funkce měnící barvu čtverečku 
 
 function barva() {
   let ctverecek;
